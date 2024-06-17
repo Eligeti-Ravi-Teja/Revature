a7a0766ce8b7c5d8149d2d9011d9faa06223c3b7
ravi <eligetiraviteja267@gmail.com>
2024-06-17 16:38:38 +0530

    master branch code changes

--- Sample.java
+++ Sample.java
@@ -2,5 +2,7 @@ public class Sample {
     public static void main(String[] args) {
         System.out.println("copy part");
         System.out.println("Sub branch code");
+
+        System.out.println("From master branch");
     }
 }
